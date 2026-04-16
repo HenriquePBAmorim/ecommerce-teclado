d724e53633d15fd061e0731902de98431b2e8eee
HenriquePBAmorim <148579518+HenriquePBAmorim@users.noreply.github.com>
2026-04-16 18:58:40 -0300

    ajustar Diagrama

--- src/main/java/br/unitins/tp1/teclado/dto/MunicipioRequestDTO.java
+++ src/main/java/br/unitins/tp1/teclado/dto/MunicipioRequestDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record MunicipioRequestDTO(
-        @NotBlank(message = "O nome do município é obrigatório") @Size(min = 2, max = 100, message = "O nome do município deve ter entre 2 e 100 caracteres") String nome,
+                @NotBlank(message = "O nome do município é obrigatório") @Size(min = 2, max = 100, message = "O nome do município deve ter entre 2 e 100 caracteres.") String nome,
 
-        @NotNull(message = "O estado (id) é obrigatório") Long idEstado) {
+                @NotNull(message = "O estado (id) é obrigatório") Long idEstado) {
 }
