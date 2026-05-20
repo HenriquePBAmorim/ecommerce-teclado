d6c7d816dd7af88a9c04b3643a5d77a5d93fd714
HenriquePBAmorim <148579518+HenriquePBAmorim@users.noreply.github.com>
2026-05-20 14:48:07 -0300

    cria PedidoResource e finaliza fluxo completo de compras

---
+++ src/main/java/br/unitins/tp1/teclado/resource/PedidoResource.java
@@ -0,0 +1,83 @@
+package br.unitins.tp1.teclado.resource;
+
+import java.util.List;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import br.unitins.tp1.teclado.dto.PedidoRequestDTO;
+import br.unitins.tp1.teclado.dto.PedidoResponseDTO;
+import br.unitins.tp1.teclado.mapper.PedidoMapper;
+import br.unitins.tp1.teclado.model.Pedido;
+import br.unitins.tp1.teclado.model.Usuario;
+import br.unitins.tp1.teclado.repository.UsuarioRepository;
+import br.unitins.tp1.teclado.service.PedidoService;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+@Path("/pedidos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class PedidoResource {
+
+    @Inject
+    PedidoService service;
+
+    @Inject
+    UsuarioRepository usuarioRepository;
+
+    @Inject
+    JsonWebToken jwt;
+
+    private Usuario getUsuarioLogado() {
+        String login = jwt.getName();
+        return usuarioRepository.findByLogin(login)
+                .orElseThrow(() -> new NotFoundException("Usuário logado não encontrado."));
+    }
+
+    @POST
+    @RolesAllowed({ "USER", "ADMIN" })
+    public Response fazerPedido(@Valid PedidoRequestDTO dto) {
+        Usuario usuarioLogado = getUsuarioLogado();
+
+        Pedido pedido = service.create(usuarioLogado.getId(), dto);
+
+        return Response.status(Status.CREATED).entity(PedidoMapper.toResponseDTO(pedido)).build();
+    }
+
+    @GET
+    @RolesAllowed({ "USER", "ADMIN" })
+    public Response buscarMeusPedidos() {
+        Usuario usuarioLogado = getUsuarioLogado();
+
+        List<PedidoResponseDTO> lista = service.findByUsuario(usuarioLogado.getId())
+                .stream()
+                .map(PedidoMapper::toResponseDTO)
+                .toList();
+
+        return Response.ok(lista).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @RolesAllowed({ "USER", "ADMIN" })
+    public Response buscarPorId(@PathParam("id") Long id) {
+        // Como sênior: Aqui seria legal validar se o pedido pertence ao usuário logado,
+        // mas para manter simples agora, vamos apenas buscar pelo ID.
+        Pedido pedido = service.findById(id);
+        if (pedido == null) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+        return Response.ok(PedidoMapper.toResponseDTO(pedido)).build();
+    }
+}
